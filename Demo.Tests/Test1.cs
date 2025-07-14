25b07d268d796b9210ba24591ae0a3fd28627481
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-07-14 11:03:37 +0200

    first commit

---
+++ Demo.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿using DemoConsole;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Calculator();
+            var a = 5;
+            var b = 4;
+            var expected = 9;
+            var calculated = c.Sum(a, b);
+            Assert.AreEqual(expected, calculated, "Sum of {0} and {1} should be {2}", a, b, expected);
+        }
+    }
+}
---
+++ DemoConsole/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoConsole;
+
+public class Calculator
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
